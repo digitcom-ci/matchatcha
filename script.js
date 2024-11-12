b8a3ed9d4f52cd7b8e040e82f76e0fff634e872d
digitcom-ci <dd.somanager@gmail.com>
2024-11-12 20:49:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -142,3 +142,202 @@ function removeItem(index) {
 document.getElementById('checkout').addEventListener('click', () => {
   window.location.href = 'checkout.html';
 });
+
+
+    function toggleMenu() {
+      const navLinks = document.getElementById('nav-links');
+      navLinks.classList.toggle('active');
+    }
+
+    function updateCartNotification() {
+      const cart = JSON.parse(localStorage.getItem('cart')) || [];
+      const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+      document.getElementById('cart-notification').innerText = totalItems;
+    }
+
+    // Fonction pour récupérer l'ID du produit depuis l'URL
+    function getProductIdFromURL() {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get('id');
+    }
+
+    const currency = "FCFA"; // Devise dynamique
+
+    const products = [
+      {
+        id: 1,
+        name: "Matcha Bio",
+        price: 7000,
+        description: "Matcha bio provenant du Japon. Idéal pour un boost naturel.",
+        variants: [
+          { name: "30g", price: 7000 },
+          { name: "50g", price: 10000 },
+          { name: "100g", price: 15000 }
+        ],
+        details: "30g",
+        container: "boîte plastique",
+        image: "images/matchapoudre.png"
+      },
+      {
+        id: 2,
+        name: "Matcha Latte",
+        price: 5000,
+        description: "Mélange de matcha, de lait (ou de lait végétal), et de sucre.",
+        variants: [
+          { name: "Classique", price: 5000 },
+          { name: "Vanille", price: 5000 },
+          { name: "Caramel", price: 5000 },
+          { name: "Miel", price: 5000 }
+        ],
+        details: "250ml",
+        container: "cup en plastique",
+        image: "images/matchalatte2.jpeg"
+      },
+      {
+        id: 3,
+        name: "Matcha Smoothie",
+        price: 7500,
+        description: "Smoothie au matcha et banane pour une texture crémeuse.",
+        variants: [
+          { name: "Matcha-Banane", price: 7500 },
+          { name: "Détox", price: 7500 }
+        ],
+        details: "250ml",
+        container: "cup en plastique",
+        image: "images/matchasmoothie.jpeg"
+      },
+      {
+        id: 4,
+        name: "Bubble Matcha",
+        price: 7000,
+        description: "Boisson unique avec matcha et perles de tapioca.",
+        variants: [
+          { name: "Lait de coco", price: 7000 },
+          { name: "Frappé avec boba", price: 7000 },
+          { name: "Sirop de fruits", price: 7000 }
+        ],
+        details: "250ml",
+        container: "cup en plastique",
+        image: "images/bubblematcha.jpeg"
+      },
+      {
+        id: 5,
+        name: "Thé Matcha",
+        price: 3000,
+        description: "Boisson traditionnelle japonaise préparée avec du matcha.",
+        details: "100ml",
+        container: "cup en plastique",
+        image: "images/thematcha1.jpeg"
+      }
+    ];
+
+    function loadProduct() {
+      const params = new URLSearchParams(window.location.search);
+      const productId = parseInt(params.get("id"));
+      const product = products.find(p => p.id === productId);
+
+      if (product) {
+        const productContainer = document.getElementById("product-container");
+        const productHTML = `
+          <img src="${product.image}" alt="${product.name}">
+          <h1>${product.name}</h1>
+          <p>${product.description}</p>
+          <p><strong>Prix:</strong> ${product.price} ${currency}</p>
+          <p><strong>Détails:</strong> ${product.details}</p>
+          <p><strong>Conditionnement:</strong> ${product.container}</p>
+        `;
+        productContainer.innerHTML = productHTML;
+
+        const variantSelect = document.getElementById("variant");
+        product.variants.forEach(variant => {
+          const option = document.createElement("option");
+          option.value = variant.name;
+          option.textContent = `${variant.name} - ${variant.price} ${currency}`;
+          variantSelect.appendChild(option);
+        });
+
+        const quantitySelect = document.getElementById("quantity");
+        for (let i = 1; i <= 10; i++) {
+          const option = document.createElement("option");
+          option.value = i;
+          option.textContent = i;
+          quantitySelect.appendChild(option);
+        }
+
+        const addToCartButton = document.getElementById("add-to-cart");
+        quantitySelect.addEventListener("change", () => {
+          addToCartButton.disabled = quantitySelect.value < 1;
+        });
+
+        loadRelatedProducts(productId);
+      }
+    }
+
+    function loadRelatedProducts(productId) {
+      const relatedProducts = products.filter(p => p.id !== productId);
+      const relatedProductsContainer = document.querySelector('.related-products-container');
+      relatedProductsContainer.innerHTML = '';
+
+      relatedProducts.forEach(product => {
+        const productHTML = `
+          <div class="related-product">
+            <img src="${product.image}" alt="${product.name}">
+            <h4>${product.name}</h4>
+            <p>${product.description}</p>
+            <p><strong>Prix:</strong> ${product.price} ${currency}</p>
+            <a href="product-details.html?id=${product.id}" class="view-details">Voir le produit</a>
+          </div>
+        `;
+        relatedProductsContainer.innerHTML += productHTML;
+      });
+    }
+
+    function addToCart() {
+      const variantSelect = document.getElementById('variant');
+      const quantitySelect = document.getElementById('quantity');
+      const variant = variantSelect.value;
+      const quantity = parseInt(quantitySelect.value);
+
+      const productId = getProductIdFromURL();
+      const product = products.find(p => p.id === parseInt(productId));
+
+      if (product) {
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        const cartItem = {
+          id: product.id,
+          name: product.name,
+          variant: variant,
+          price: product.price,
+          quantity: quantity
+        };
+
+        cart.push(cartItem);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        document.getElementById('message').style.display = 'block';
+        updateCartNotification();
+      }
+    }
+
+    loadProduct();
+
+    // Fonction pour gérer le clic sur l'icône de connexion
+    function handleLoginClick() {
+        const isLoggedIn = localStorage.getItem('loggedInUser');
+        if (isLoggedIn) {
+            const confirmLogout = confirm("Voulez-vous vous déconnecter ?");
+            if (confirmLogout) {
+                logout();
+            }
+        } else {
+            window.location.href = 'login.html';
+        }
+    }
+
+    // Fonction de déconnexion
+    function logout() {
+        localStorage.removeItem('loggedInUser');
+        alert("Déconnexion réussie.");
+        window.location.href = 'index.html';
+    }
+    });
+    
